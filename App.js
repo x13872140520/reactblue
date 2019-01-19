7cb44736f0a95b075c120235ff46b28cdf9a1cdc
zd <zhoudi@oureman.com>
2019-01-19 13:22:05 +0800

    add blue

--- App.js
+++ App.js
@@ -7,7 +7,10 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View,FlatList} from 'react-native';
+import BleManager from 'react-native-ble-manager';
+let devices='0'
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,12 +21,59 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+    state = {
+    devices:null,
+    name:null,
+    value:null,
+    status:''
+  };
+  componentDidMount(){
+    BleManager.start().then(()=>{
+           //检查蓝牙打开状态，初始化蓝牙后检查当前蓝牙有没有打开
+           BleManager.checkState();
+           BleManager.enableBluetooth().then(() => {
+                                        this.setState({
+                                             status:'蓝牙已经打开！正在扫描中...目前有以下设备'
+                                                                 })
+                                           BleManager.scan([], 5, false).then((e,index,value) => {
+                                                      this.setState({
+                                                        devices:this.state.devices.push(1)
+
+                                                      })
+                                              });
+                                         }).catch((error) => {
+                                             this.setState({
+                                                   status:'蓝牙已经被关闭！'
+                                                    })
+                                             });
+
+       }).catch(error=>{
+                        this.setState({
+                             status:'蓝牙初始化失败！'
+                           })
+
+       });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>{this.state.status}</Text>
+         <Text style={styles.welcome}>devices:{this.state.devices}</Text>
+         <Text style={styles.welcome}>name:{this.state.name}</Text>
+         <Text style={styles.welcome}>value:{this.state.value}</Text>
+          <FlatList
+                   data={[
+                     {key: 'Devin'},
+                     {key: 'Jackson'},
+                     {key: 'James'},
+                     {key: 'Joel'},
+                     {key: 'John'},
+                     {key: 'Jillian'},
+                     {key: 'Jimmy'},
+                     {key: 'Julie'},
+                   ]}
+                   renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+                 />
       </View>
     );
   }
